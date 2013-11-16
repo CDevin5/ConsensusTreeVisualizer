8643835c904d83e7f640c586035004747457298b
Coline Devin <cdevin@hmc.edu>
2013-11-15 20:27:22 -0800

    Removed figtree

--- figtree-read-only/src/figtree/treeviewer/AnnotationsListener.java
+++
@@ -1,9 +0,0 @@
-package figtree.treeviewer;
-
-/**
- * @author Andrew Rambaut
- * @version $Id: AnnotationsListener.java,v 1.1 2006/08/09 19:28:42 rambaut Exp $
- */
-public interface AnnotationsListener {
-    void annotationsChanged();
-}
